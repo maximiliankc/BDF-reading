37ae413fb4d9933867c14d58426f76e3a19b8f30
maximiliankc <54353771+maximiliankc@users.noreply.github.com>
2021-10-31 22:02:39 +1100

    Updated reader to spit out a csv file

--- reader.py
+++ reader.py
@@ -1,5 +1,5 @@
 import mne
-import os
+import csv
 import sys
 
 
@@ -8,11 +8,22 @@ def main():
         print("Usage: reader.py [filename]")
     else:
         file = sys.argv[1]
+        name = file.split('.')[0]
+        print(f"Name: {name}")
         print(file)
         data = mne.io.read_raw_bdf(sys.argv[1])
-        for d in data.info:
-            print(f"{d}: {data.info[d]}")
-        print(f"number of electrodes: {len(data.info.ch_names)}")
+        channels = data.info['ch_names']
+        print(channels)
+        outfile = name + '.csv'
+        with open(outfile, 'w') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(['Time'] + channels)
+            values, time = data.get_data(return_times=True)
+            print(values.shape)
+            for n in range(len(time)):
+                csvwriter.writerow([time[n]] + list(values[:,n]))
+
+        
 
 if __name__ == "__main__":
     main()
